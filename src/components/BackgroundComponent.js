d8bcbbd5d93efacb82998dd72bb97a7e5d477464
Arianna <95667502+aadams504@users.noreply.github.com>
2022-07-01 15:38:40 -0700

    add social links

--- src/components/BackgroundComponent.js
+++ src/components/BackgroundComponent.js
@@ -2,23 +2,38 @@ import React from 'react';
 import '../styles/Background.scss';
 import {Link} from 'react-scroll';
 
+import {FaGithub, FaLinkedin, FaMailBulk} from 'react-icons/fa';
+
 // import Spaceship from '../assets/images/ufo.png';
 import bgImage from '../assets/images/bg.jpg';
 
 const BackgroundComponent = () => {
   return (
     <div className='hero'>
-          <img id='image' src={bgImage} alt='' />
+        <img id='image' src={bgImage} alt='' />
+
+        <div className="social">
+          <a href='https://www.linkedin.com/in/adams-arianna'>
+            <FaLinkedin className='social-icon' size={40} style={{color: '#fff', marginRight: '1rem', padding: '10px'}} />
+          </a>
+          <a href='https://github.com/aadams504'>
+            <FaGithub className='social-icon' size={40} style={{color: '#fff', marginRight: '1rem', padding: '10px'}} />
+          </a>
+          <a href='"mailto:contact@arianna.codes"'>
+            <FaMailBulk className='social-icon' size={40} style={{color: '#fff', marginRight: '1rem', padding: '10px'}} />
+          </a>
+        </div>
 
         <div className="content">
-          <h1>Arianna Adams</h1>
+          <h1>Hi, I'm Arianna</h1>
           <p>
-            Software Engineer
+            I'm a software developer
           </p>
         <div className="buttons">
             <Link to='portfolio' smooth={true} className='btn'>Check Out My Work</Link>
-            <Link to='contact' smooth={true} className='btn btn-light'>Hire Me</Link>
+            <Link to='contact' smooth={true} className='btn btn-light'>Contact Me</Link>
         </div>
+        
     </div>
     </div>
   )
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,25 +14,29 @@ const Portfolio = () => {
           <a href="https://main--papaya-biscochitos-d63991.netlify.app/" target="_blank" rel="noopener noreferrer">
           <img src={Clothing} alt="" />
           </a>
-          <div className="card__head">Crwn Clothing</div>
+          <div className="card__head">Crwn Clothing
+          <p style={{color: 'white'}}>React, Redux, Firebase, Stripe</p></div>
         </div>
         <div className="card">
           <a href="https://sunny-kelpie-ad37da.netlify.app" target="_blank" rel="noopener noreferrer">
           <img src={Alan} alt="" /></a>
 
-          <div className="card__head">AI News</div>
+          <div className="card__head">AI News
+          <p style={{color: 'white'}}>React, Alan API</p></div>
         </div>
         <div className="card">
           <a href="https://playful-vacherin-7cf0b9.netlify.app" target="_blank" rel="noopener noreferrer">
           <img src={Game} alt="" /></a>
 
-          <div className="card__head">Fighting Game</div>
+          <div className="card__head">Road Brawlers
+          <p style={{color: 'white'}}>Javascript</p></div>
+          
         </div>
         <div className="card">
           <a href="https://elegant-raindrop-060bbe.netlify.app" target="_blank" rel="noopener noreferrer">
-          <img src={Movies} alt="" /></
-          a>
-          <div className="card__head">Movie Ratings App</div>
+          <img src={Movies} alt="" /></a>
+          <div className="card__head">Movie Ratings App
+          <p style={{color: 'white'}}>HTML, CSS, Javascript</p></div>
         </div>
       </div>
     </div>

2bdf22e853c1145b3f7ffd204caa2546eef1214a
Arianna <95667502+aadams504@users.noreply.github.com>
2022-07-12 02:04:24 -0700

    update portfolio section

--- src/components/AnnouncementsComponent.js
+++ src/components/AnnouncementsComponent.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import '../index.scss'
+import "../index.scss";
 
 import HtmlIcon from "../assets/images/html-5.png";
 import CssIcon from "../assets/images/css.png";
@@ -14,41 +14,33 @@ import JavaIcon from "../assets/images/java.png";
 import GithubIcon from "../assets/images/github.png";
 import CplusIcon from "../assets/images/c-.png";
 import SqlIcon from "../assets/images/database.png";
-import DockerIcon from "../assets/images/docker.png";
-import PostgresIcon from "../assets/images/postgre.png";
-import MySQLIcon from "../assets/images/mysql.png";
-
 
 const AnnouncementsComponent = () => {
   return (
     <>
-      <div className='container announcements'>
-          <h2>Technologies</h2>
+      <div className="container announcements">
+        <h2>Technologies</h2>
 
-          <div className='tech-container'>
-            <img src={PythonIcon} alt="python" />
-            <img src={JavaIcon} alt="Java" />
-            <img src={CplusIcon} alt="C++" />
-            <img src={HtmlIcon} alt="html" />
-            <img src={CssIcon} alt="css" />
-            <img src={SassIcon} alt="sass" />
-            <img src={GithubIcon} alt="github" />
-          </div>
+        <div className="tech-container">
+          <img src={PythonIcon} alt="python" />
+          <img src={JavaIcon} alt="Java" />
+          <img src={CplusIcon} alt="C++" />
+          <img src={HtmlIcon} alt="html" />
+          <img src={CssIcon} alt="css" />
+          <img src={SassIcon} alt="sass" />
+          <img src={GithubIcon} alt="github" />
+        </div>
 
-          <div className='tech-container'>
-              <img src={JsIcon} alt="javascript" />
-              <img src={ReactIcon} alt="react" />
-              <img src={NodeIcon} alt="node" />
-              <img src={TypeScriptIcon} alt="typescript" />
-              <img src={SqlIcon} alt="sql" />
-              <img src={MySQLIcon} alt="mysql" />
-              <img src={DockerIcon} alt="docker" />
-              <img src={PostgresIcon} alt="postgres" />
-          </div>
-         
+        <div className="tech-container">
+          <img src={JsIcon} alt="javascript" />
+          <img src={ReactIcon} alt="react" />
+          <img src={NodeIcon} alt="node" />
+          <img src={TypeScriptIcon} alt="typescript" />
+          <img src={SqlIcon} alt="sql" />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AnnouncementsComponent
+export default AnnouncementsComponent;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,46 +1,59 @@
-import '../styles/Portfolio.scss';
+import "../styles/Portfolio.scss";
 
-import Movies from '../assets/images/movies.png';
-import Game from '../assets/images/game.png';
-import Alan from '../assets/images/alan.png';
-import Clothing from '../assets/images/clothing.png';
+import Movies from "../assets/images/movies.png";
+import Game from "../assets/images/game.png";
+import Clothing from "../assets/images/clothing.png";
 
 const Portfolio = () => {
   return (
-    <div id="portfolio" className='container announcements'>
+    <div id="portfolio" className="container announcements">
       <h2>Portfolio</h2>
       <div className="portfolio">
         <div className="card">
-          <a href="https://main--papaya-biscochitos-d63991.netlify.app/" target="_blank" rel="noopener noreferrer">
-          <img src={Clothing} alt="" />
+          <a
+            href="https://main--papaya-biscochitos-d63991.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Clothing} alt="" />
           </a>
-          <div className="card__head">Crwn Clothing
-          <p style={{color: 'white', fontSize: '16px'}}>React, Redux, Firebase, Stripe API</p></div>
+          <div className="card__head">
+            Crwn Clothing
+            <p style={{ color: "white", fontSize: "16px" }}>
+              React, Redux, Firebase, Stripe API
+            </p>
+          </div>
         </div>
         <div className="card">
-          <a href="https://sunny-kelpie-ad37da.netlify.app" target="_blank" rel="noopener noreferrer">
-          <img src={Alan} alt="" /></a>
+          <a
+            href="https://playful-vacherin-7cf0b9.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Game} alt="" />
+          </a>
 
-          <div className="card__head">AI News
-          <p style={{color: 'white'}}>React, Alan API</p></div>
+          <div className="card__head">
+            Road Brawlers
+            <p style={{ color: "white" }}>Javascript</p>
+          </div>
         </div>
         <div className="card">
-          <a href="https://playful-vacherin-7cf0b9.netlify.app" target="_blank" rel="noopener noreferrer">
-          <img src={Game} alt="" /></a>
-
-          <div className="card__head">Road Brawlers
-          <p style={{color: 'white'}}>Javascript</p></div>
-          
-        </div>
-        <div className="card">
-          <a href="https://elegant-raindrop-060bbe.netlify.app" target="_blank" rel="noopener noreferrer">
-          <img src={Movies} alt="" /></a>
-          <div className="card__head">Movie Ratings App
-          <p style={{color: 'white'}}>HTML, CSS, Javascript</p></div>
+          <a
+            href="https://elegant-raindrop-060bbe.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Movies} alt="" />
+          </a>
+          <div className="card__head">
+            Movie Ratings App
+            <p style={{ color: "white" }}>HTML, CSS, Javascript</p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;

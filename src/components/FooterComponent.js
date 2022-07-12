bdb3bd9aebfa44d828c15247d0c4792976092364
Arianna <95667502+aadams504@users.noreply.github.com>
2022-07-12 11:22:42 -0700

    edit footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import '../styles/Footer.scss';
+import React from "react";
+import "../styles/Footer.scss";
 // import {FaFacebook, FaLinkedin, FaMailBulk, FaPhone, FaSearchLocation, FaTwitter} from 'react-icons/fa';
 
 const FooterComponent = () => {
   return (
-    <div className='footer'>
-        <div className="footer-container">
-            <div className="left">
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left"></div>
 
-            </div>
-                
-            <div className="right">
-            </div>
-        </div>
-        <div className="center copy">
-                <p>
-                &copy; 2022 Arianna Adams |
-                Background Image from <a href='http://www.unsplash.com'>Unsplash</a>
-                </p>
-            </div>
+        <div className="right"></div>
+      </div>
+      <div className="center copy">
+        <p>
+          Created with React | Background Image from{" "}
+          <a href="http://www.unsplash.com">Unsplash</a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
